7967562dc7fa5a50803b36952d10f4fcd7b016c0
Amanda S <shottsan@dukes.jmu.edu>
2018-06-27 03:22:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ var string = 'hello'
 function shout (string) {
   return string.toUpperCase()
 }
+
+function logShout ()
+{
+  console.log(shout)
+}
+
+
+function whisper (string) {
+  return string.toLowerCase()
+}
