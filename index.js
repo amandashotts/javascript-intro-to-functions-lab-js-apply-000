15fe7880956e3a7cc7b5cde1c2180e7e72605033
Amanda S <shottsan@dukes.jmu.edu>
2018-06-27 03:26:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ function sayHiToGrandma (string) {
   if (string === string.toLowerCase()) {
     return "I can't hear you!"
 }
-else if (string === "I love you, Grandma." {
+else if (string === "I love you, Grandma.") {
   return "I love you, too."
 }}
